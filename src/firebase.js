f6ec23b8bffe637c7daf97c76121313ea696e0a2
Allwan09 <fahmeetwalib@gmail.com>
2025-07-21 14:38:08 +0300

    new file:   .firebase/hosting.cHVibGlj.cache 	new file:   .firebaserc 	new file:   firebase.json 	modified:   package-lock.json 	modified:   package.json 	new file:   public/404.html 	new file:   src/firebase.js

---
+++ src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCtvpAmfyzhVNeJHKM414cigBdeLpgA7nQ",
+  authDomain: "fadak-tours.firebaseapp.com",
+  projectId: "fadak-tours",
+  storageBucket: "fadak-tours.firebasestorage.app",
+  messagingSenderId: "450527856478",
+  appId: "1:450527856478:web:1ff7732cc3799c0326f1cf",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
