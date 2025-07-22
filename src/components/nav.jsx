2b69cfe11f17d086f9d2d5a990deb85c8d53df0c
Allwan09 <fahmeetwalib@gmail.com>
2025-07-22 14:48:56 +0300

    new file:   .firebase/hosting.YnVpbGQ.cache 	modified:   firebase.json 	modified:   src/components/nav.css 	modified:   src/components/nav.jsx 	modified:   src/pages/Services.css 	modified:   src/pages/Services.jsx 	modified:   src/pages/Services/Marine.css

--- src/components/nav.jsx
+++ src/components/nav.jsx
@@ -9,6 +9,10 @@ function Nav() {
     setMenuOpen(!menuOpen);
   };
 
+  const closeMenu = () => {
+    setMenuOpen(false);
+  };
+
   return (
     <div className="header">
       <div className="website-name">FADAK TRAVELS.com</div>
@@ -22,87 +26,137 @@ function Nav() {
       <nav className={menuOpen ? "nav open" : "nav"}>
         <ul>
           <li>
-            <Link to="/">Home</Link>
+            <Link to="/" onClick={closeMenu}>
+              Home
+            </Link>
             <ul className="dropdown">
               <li>
-                <Link to="/about">About Us</Link>
+                <Link to="/about" onClick={closeMenu}>
+                  About Us
+                </Link>
               </li>
               <li>
-                <Link to="/blog/news">Latest News</Link>
+                <Link to="/blog/news" onClick={closeMenu}>
+                  Latest News
+                </Link>
               </li>
               <li>
-                <Link to="/services/featured">Featured Products</Link>
+                <Link to="/services/featured" onClick={closeMenu}>
+                  Featured Products
+                </Link>
               </li>
               <li>
-                <Link to="/blog">Blog</Link>
+                <Link to="/blog" onClick={closeMenu}>
+                  Blog
+                </Link>
               </li>
             </ul>
           </li>
           <li>
-            <Link to="/services">Locations</Link>
+            <Link to="/services" onClick={closeMenu}>
+              Locations
+            </Link>
             <ul className="dropdown">
               <li>
-                <Link to="/services/marine-parks">Marine Parks</Link>
+                <Link to="/services/marine-parks" onClick={closeMenu}>
+                  Marine Parks
+                </Link>
               </li>
               <li>
-                <Link to="/services/national-parks">National Parks</Link>
+                <Link to="/services/national-parks" onClick={closeMenu}>
+                  National Parks
+                </Link>
               </li>
               <li>
-                <Link to="/services/old-town-tours">Old Town Tours</Link>
+                <Link to="/services/old-town-tours" onClick={closeMenu}>
+                  Old Town Tours
+                </Link>
               </li>
             </ul>
           </li>
           <li>
-            <Link to="/packages">Packages</Link>
+            <Link to="/packages" onClick={closeMenu}>
+              Packages
+            </Link>
             <ul className="dropdown">
               <li>
-                <Link to="/packages/family">Family Tours</Link>
+                <Link to="/packages/family" onClick={closeMenu}>
+                  Family Tours
+                </Link>
               </li>
               <li>
-                <Link to="/packages/adventure">Adventure Tours</Link>
+                <Link to="/packages/adventure" onClick={closeMenu}>
+                  Adventure Tours
+                </Link>
               </li>
               <li>
-                <Link to="/packages/luxury">Halal Tours</Link>
+                <Link to="/packages/luxury" onClick={closeMenu}>
+                  Halal Tours
+                </Link>
               </li>
               <li>
-                <Link to="/packages/group">Group Tours</Link>
+                <Link to="/packages/group" onClick={closeMenu}>
+                  Group Tours
+                </Link>
               </li>
             </ul>
           </li>
           <li>
-            <Link to="/about">About Us</Link>
+            <Link to="/about" onClick={closeMenu}>
+              About Us
+            </Link>
             <ul className="dropdown">
               <li>
-                <Link to="/about#mission">Company Mission</Link>
+                <Link to="/about#mission" onClick={closeMenu}>
+                  Company Mission
+                </Link>
               </li>
               <li>
-                <Link to="/about#values">Company Values</Link>
+                <Link to="/about#values" onClick={closeMenu}>
+                  Company Values
+                </Link>
               </li>
               <li>
-                <Link to="/about#history">Company History</Link>
+                <Link to="/about#history" onClick={closeMenu}>
+                  Company History
+                </Link>
               </li>
               <li>
-                <Link to="/about#team">Meet the Team</Link>
+                <Link to="/about#team" onClick={closeMenu}>
+                  Meet the Team
+                </Link>
               </li>
             </ul>
           </li>
           <li>
-            <Link to="/contact">Contact Us</Link>
+            <Link to="/contact" onClick={closeMenu}>
+              Contact Us
+            </Link>
             <ul className="dropdown">
               <li>
-                <Link to="/contact#call">Call us</Link>
+                <Link to="/contact#call" onClick={closeMenu}>
+                  Call us
+                </Link>
               </li>
               <li>
-                <Link to="/contact#email">Email us</Link>
+                <Link to="/contact#email" onClick={closeMenu}>
+                  Email us
+                </Link>
               </li>
               <li>
-                <Link to="/contact#facebook">Facebook</Link>
+                <Link to="/contact#facebook" onClick={closeMenu}>
+                  Facebook
+                </Link>
               </li>
               <li>
-                <Link to="/contact#instagram">Instagram</Link>
+                <Link to="/contact#instagram" onClick={closeMenu}>
+                  Instagram
+                </Link>
               </li>
               <li>
-                <Link to="/contact#tiktok">TikTok</Link>
+                <Link to="/contact#tiktok" onClick={closeMenu}>
+                  TikTok
+                </Link>
               </li>
             </ul>
           </li>
--- src/pages/Services.jsx
+++ src/pages/Services.jsx
@@ -1,82 +1,90 @@
 import React from "react";
-import "./Services.css";
-import "bootstrap/dist/css/bootstrap.min.css";
+import { Container, Row, Col, Card } from "react-bootstrap";
 import { Link } from "react-router-dom";
+import "./Services.css";
 
 function Services() {
   return (
-    <div className="services">
-      <div className="services-header text-center my-5">
+    <div className="services py-5">
+      {/* Header Section */}
+      <Container className="text-center mb-5 my-5">
         <h1>Our Services</h1>
         <p>Explore our safari packages, marine park tours, and more.</p>
-      </div>
+      </Container>
 
-      <div className="container">
-        <div className="row g-4">
+      {/* Services Cards */}
+      <Container>
+        <Row className="gy-4 gx-4 justify-content-center shift-right">
           {/* Marine Park Tours */}
-          <div className="col-md-4">
-            <Link
-              to="/services/marine-parks"
-              className="text-decoration-none text-dark"
-            >
-              <div className="service-card h-100">
-                <img
+          <Col xs={12} sm={6} md={4} lg={3} className="d-flex">
+            <Card className="service-card flex-fill d-flex flex-column">
+              <Link
+                to="/services/marine-parks"
+                className="text-decoration-none text-dark"
+              >
+                <Card.Img
+                  variant="top"
                   src="/turtle.jpg"
-                  alt="Marine Park"
-                  className="img-fluid"
+                  className="service-img"
                 />
-                <h3>Marine Park Tours</h3>
-                <p>
-                  Explore the breathtaking beauty of our marine parks with
-                  expert guides.
-                </p>
-              </div>
-            </Link>
-          </div>
+                <Card.Body className="d-flex flex-column">
+                  <Card.Title>Marine Park Tours</Card.Title>
+                  <Card.Text>
+                    Explore the breathtaking beauty of our marine parks with
+                    expert guides.
+                  </Card.Text>
+                </Card.Body>
+              </Link>
+            </Card>
+          </Col>
 
           {/* Old Town Tours */}
-          <div className="col-md-4">
-            <Link
-              to="/services/old-town-tours"
-              className="text-decoration-none text-dark"
-            >
-              <div className="service-card h-100">
-                <img
+          <Col xs={12} sm={6} md={4} lg={3} className="d-flex">
+            <Card className="service-card flex-fill d-flex flex-column">
+              <Link
+                to="/services/old-town-tours"
+                className="text-decoration-none text-dark"
+              >
+                <Card.Img
+                  variant="top"
                   src="/old-town2.jpg"
-                  alt="Old Town Tour"
-                  className="img-fluid"
+                  className="service-img"
                 />
-                <h3>Old Town Tours</h3>
-                <p>
-                  Join us for a once-in-a-lifetime old town experience and see
-                  history up close.
-                </p>
-              </div>
-            </Link>
-          </div>
+                <Card.Body className="d-flex flex-column">
+                  <Card.Title>Old Town Tours</Card.Title>
+                  <Card.Text>
+                    Join us for a once-in-a-lifetime old town experience and see
+                    history up close.
+                  </Card.Text>
+                </Card.Body>
+              </Link>
+            </Card>
+          </Col>
 
           {/* National Park Adventures */}
-          <div className="col-md-4">
-            <Link
-              to="/services/national-parks"
-              className="text-decoration-none text-dark"
-            >
-              <div className="service-card h-100">
-                <img
+          <Col xs={12} sm={6} md={4} lg={3} className="d-flex">
+            <Card className="service-card flex-fill d-flex flex-column">
+              <Link
+                to="/services/national-parks"
+                className="text-decoration-none text-dark"
+              >
+                <Card.Img
+                  variant="top"
                   src="/lions.jpg"
-                  alt="National Park"
-                  className="img-fluid"
+                  className="service-img"
                 />
-                <h3>National Park Adventures</h3>
-                <p>
-                  Discover the wonders of nature with our guided national park
-                  tours.
-                </p>
-              </div>
-            </Link>
-          </div>
-        </div>
-      </div>
+                <Card.Body className="d-flex flex-column">
+                  <Card.Title>National Park Adventures</Card.Title>
+                  <Card.Text>
+                    Discover the wonders of nature with our guided national park
+                    tours.
+                  </Card.Text>
+                </Card.Body>
+              </Link>
+            </Card>
+          </Col>
+        </Row>
+      </Container>
     </div>
   );
 }
